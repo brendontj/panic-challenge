4b2b949684bc048eddbd7e0808b57b3b27a49834
Brendon Henrique <brendonhps@gmail.com>
2020-02-05 22:04:53 -0300

    Create the structure of web portal

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './global.css';
+import './sidebar.css';
 
 function App() {
+  const { children } = this.props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Menu />
+      <Sidebar />
+      <section className='content'>
+        {children}
+      </section>
     </div>
   );
 }
