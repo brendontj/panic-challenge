1ff8d3aea59438d2f8689ff248cfde544c2ef5fd
Brendon Henrique <brendonhps@gmail.com>
2020-02-07 12:35:41 -0300

    Menu created with redirect to cadastro page

--- front_end/src/components/Menu/index.js
+++ front_end/src/components/Menu/index.js
@@ -1,44 +1,67 @@
 // Default React Imports
 import React from 'react'
 import './styles.css'
+import {Link} from 'react-router-dom';
 
 // Material Ui Imports
 import { Container } from '@material-ui/core'
-import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import ButtonBase from '@material-ui/core/ButtonBase'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 // Material Ui Styles
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
-  img: {
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+    img: {
     margin: 'auto',
-    marginLeft: '20%',
+    marginLeft: '5px%',
     display: 'left',
-    maxWidth: '50%',
-    maxHeight: '50%'
+    maxWidth: '30%',
+    maxHeight: '30%'
   }
-}))
+}));
+
 
 //Component
-export default function Menu () {
+export default function Menu() {
   const classes = useStyles()
+
+ 
   return (
     <Container maxWidth='xl'>
       <div className={classes.root}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <img
+        <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
+          <Toolbar>
+          <img
               className={classes.img}
               alt='logo-panic'
               src='../logoInverted.png'
             />
-          </Grid>
-        </Grid>
+            <Typography variant="h4" className={classes.title}>
+              Intership Challenge
+          </Typography>
+            <Button 
+              component = {Link} to="/cadastro/"
+              color="inherit" 
+            >
+              Inserir Candidato
+            </Button>
+          </Toolbar>
+        </AppBar>
       </div>
+
+
     </Container>
-  )
+  );
 }
